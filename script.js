8c86540b4fcbee401b3909bd11188d44d16bc66e
Kim Tompkins <kimberli.tompkins@outlook.com>
2020-03-31 09:43:36 -0600

    fixed quizQuestions object, only writting one question at a time now

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var countdown = $("#countDown");
 var count = 60;
+var questionList = $("#questionList");
+var currentQuestion = 0;
 
+var quizQuestions = [
+    {"question":"What is your name?",
+     "choices":["Kim","Bob"],
+        "answer": ["Correct","incorrect"]}
+          
+    ,
+    {"question":"What is your age?",
+     "choices":["21","42"],
+      "answer":["Correct","incorrect"]}
+    
+];
+
+// var choices1 = ["Kim","Bob"];
 
 //start the timer
 var timeLeft = 10;
 $("#startButton").click(function(){
-    
-
+    $("#startButton").hide();
+    writeQuestions(currentQuestion);
     var timeInterval = setInterval(function() {
         countdown.text(timeLeft + " seconds remaining");
       timeLeft--;
@@ -15,14 +30,36 @@ $("#startButton").click(function(){
         countdown.text("") ;
         clearInterval(timeInterval);
       }
-      else{
-        dotheTest();
-      }
+      
   
     }, 1000);
     
   });
 
-  function dotheTest(){
-      $("#questions").text(timeLeft)
-  };
+//   function dotheTest(){
+//       $("#questions").text(timeLeft)
+//       question1();
+    //   $.each(quizQuestions, function(key, value) {
+    //       writeQuestion(value);
+       
+    //   });
+//   };
+  function writeQuestions(questionNum){
+    
+    questionList.append("<p>"+quizQuestions[questionNum].question+"</P>");  
+   
+     for(var i=0;i<quizQuestions[questionNum].choices.length;i++){
+    
+       questionList.append('<button type="button" id="choiceSelected" class="list-group-item list-group-item-action">'+quizQuestions[questionNum].choices[i]+'</button>');
+      
+     };
+    
+   
+    // <button type="button" class="list-group-item list-group-item-action ">
+    //   Cras justo odio
+    // </button>
+    // <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+    // <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+    // <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+    // <button type="button" class="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
+  }
